dd87bcb3134b46c3d7595e831019d4bcccb905a8
Sebastian C <tatancito1410@hotmail.com>
2023-12-03 18:17:27 -0500

    Continuacion traduccion sin terminar

--- assets/js/traducciones.js
+++ assets/js/traducciones.js
@@ -1,24 +1,54 @@
 let idiomaActual = 'es';
 
-function cambiarIdioma() {
-  console.log(idiomaActual)
-    idiomaActual = idiomaActual === 'es' ? 'en' : 'es';
-    $('#titulo').text(getTextoTraducido('titulo'));
-    // $('#parrafo').text(getTextoTraducido('parrafo'));
+function obtenerIdiomaActual() {
+  console.log('Idioma Obtenido.')
+  return localStorage.getItem('idiomaActual') || 'es';
 }
 
+function establecerIdiomaActual(idioma) {
+  console.log('Idioma Seteado.')
+  localStorage.setItem('idiomaActual', idioma);
+  actualizarTraducciones();
+}
+
+function cambiarIdioma() {
+  console.log('Cambiando...')
+  const idiomaActual = obtenerIdiomaActual();
+  const nuevoIdioma = idiomaActual === 'es' ? 'en' : 'es';
+  establecerIdiomaActual(nuevoIdioma);
+}
+
+
 function getTextoTraducido(clave) {
-  console.log(clave)
     const traducciones = {
         'titulo': {
             'es': 'Bienvenido',
             'en': 'Welcome'
-        }
+        },
+        // 'parrafo': {
+        //     'es': 'Este es un ejemplo de página multilingüe.',
+        //     'en': 'This is an example of a multilingual page.'
+        // }
+        // Agrega más traducciones según sea necesario
     };
 
     return traducciones[clave][idiomaActual];
 }
 
-$(document).ready(function() {
-    $('#titulo').load('index.html #titulo');
-});
+function actualizarTraducciones() {
+  console.log('Actualizando Idioma...')
+  // Actualiza todas las traducciones en todas las páginas
+  const elementosTitulo = document.querySelectorAll('.titulo');
+  // const elementosParrafo = document.querySelectorAll('.parrafo');
+  console.log(elementosTitulo)
+  elementosTitulo.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('titulo');
+    console.log(getTextoTraducido('titulo'))
+    console.log('Idioma Seteado')
+
+  });
+
+    // elementosParrafo.forEach(function(elemento) {
+    //     elemento.innerText = getTextoTraducido('parrafo');
+    // });
+}
