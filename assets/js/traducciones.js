945b165cb159bea12180bb1d3a30ff72482e8134
Sebastian C <tatancito1410@hotmail.com>
2023-12-03 22:52:21 -0500

    Traducciones

--- assets/js/traducciones.js
+++ assets/js/traducciones.js
@@ -36,12 +36,46 @@ function getTextoTraducido(clave) {
             'es': '¿Necesitas Ayuda?',
             'en': '¿Need Help?'
         },
-        
-        // 'parrafo': {
-        //     'es': 'Este es un ejemplo de página multilingüe.',
-        //     'en': 'This is an example of a multilingual page.'
-        // }
-        // Agrega más traducciones según sea necesario
+        'config': {
+            'es': 'Configuraciones',
+            'en': 'Settings'
+        },
+        'acerca': {
+            'es': 'Acerca de nosotros',
+            'en': 'About us'
+        },
+        'acerca2': {
+            'es': 'Somos una empresa dedicada a la creacion de aplicaciones para restaurantes, en las cuales pueden tener el control de su negocio y facilitar enormemente su gestión en el día a día con lo que respecta a su operación.',
+            'en': 'We are a company dedicated to creating applications for restaurants, in which they can have control of their business and greatly facilitate their day-to-day management with regard to their operation.'
+        },
+        'acerca3': {
+            'es': 'Easy Service se encarga de gestionar los pedidos de tus clientes, los cuales tienen la libertad de analizar rápida y fácilmente, teniendo asi la oportunidad de liberar cargas a tus empleados y hacer el tiempo de trabajo mas eficáz.',
+            'en': 'Easy Service is responsible for managing your clients orders, which they have the freedom to analyze quickly and easily, thus having the opportunity to free up your employees burdens and make work time more efficient.'
+        },
+        'dark': {
+          'es': 'Modo Oscuro',
+          'en': 'Dark Mode'
+        },
+        'carrito': {
+          'es': 'Ir al carrito',
+          'en': 'Go to cart'
+        },
+        'user': {
+          'es': 'Ingrese su usuario y contraseña',
+          'en': 'Type your user and password'
+        },
+        'pass': {
+          'es': 'Contraseña',
+          'en': 'Password'
+        },
+        'register': {
+          'es': 'Registrarse',
+          'en': 'Sign In'
+        },
+        'confirm-pass': {
+          'es': 'Registrarse',
+          'en': 'Sign In'
+        },
     };
 
     return traducciones[clave][idiomaActual];
@@ -54,6 +88,17 @@ function actualizarTraducciones() {
   const elementosLlamar = document.querySelectorAll('.llamar');
   const elementosMenu = document.querySelectorAll('.menu');
   const elementosAyuda = document.querySelectorAll('.ayuda');
+  const elementosConfig = document.querySelectorAll('.config');
+  const elementosAcerca = document.querySelectorAll('.acerca');
+  const elementosAcerca2 = document.querySelectorAll('.acerca2');
+  const elementosAcerca3 = document.querySelectorAll('.acerca3');
+  const elementosDark = document.querySelectorAll('.dark');
+  const elementosCart = document.querySelectorAll('.cart');
+  const elementosUser = document.querySelectorAll('.user');
+  const elementosPass = document.querySelectorAll('.pass');
+  const elementosRegister = document.querySelectorAll('.register');
+  const elementosConfirm = document.querySelectorAll('.confirm-pass');
+  
   elementosTitulo.forEach(function (elemento) {
     elemento.innerText = getTextoTraducido('bienvenido');
   });
@@ -65,6 +110,36 @@ function actualizarTraducciones() {
   });
   elementosAyuda.forEach(function (elemento) {
     elemento.innerText = getTextoTraducido('ayuda');
+  });
+  elementosConfig.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('config');
+  });
+  elementosAcerca.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('acerca');
+  });
+  elementosAcerca2.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('acerca2');
+  });
+  elementosAcerca3.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('acerca3');
+  });
+  elementosDark.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('dark');
+  });
+  elementosCart.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('cart');
+  });
+  elementosUser.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('user');
+  });
+  elementosPass.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('pass');
+  });
+  elementosRegister.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('register');
+  });
+  elementosConfirm.forEach(function (elemento) {
+    elemento.innerText = getTextoTraducido('confirm-pass');
   });
 
     // elementosParrafo.forEach(function(elemento) {
