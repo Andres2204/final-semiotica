42b4b64d15c03e0b9f813378645b141f838f8809
DavidAntolinez <128693961+DavidAntolinez@users.noreply.github.com>
2023-12-03 23:37:21 -0500

    Add files via upload

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,28 @@
 $("#login").click(function (e) { 
     e.preventDefault();
-var email = $("#typeEmailX").val();
-var password = $("#typePasswordX").val();
+var nombreUsuarioLogin = $("#typeEmailX").val();
+var contrasenaLogin = $("#typePasswordX").val();
 // Checking for blank fields.
-if( email =='' || password ==''){
+if( nombreUsuarioLogin =='' || contrasenaLogin ==''){
 alert("por favor rellena todos los espacios...!!!!!!");
 }else{
+    // Recuperar usuarios almacenados en localStorage
+    var usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    // Verificar si el usuario y la contraseña coinciden con alguno almacenado
+    var usuarioValido = usuariosGuardados.find(function(usuario) {
+        return usuario.nombreUsuario === nombreUsuarioLogin && usuario.contrasena === contrasenaLogin;
+    });
+
+    if (usuarioValido) {
+        alert('Inicio de sesión exitoso');
+        // Puedes redirigir a otra página o realizar acciones adicionales después del inicio de sesión
+        localStorage.setItem("login",nombreUsuarioLogin);
+        console.log(localStorage.getItem('usuarios'));
+        location.href = "usuario.html";
+    } else {
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    }
 }
 });
 
